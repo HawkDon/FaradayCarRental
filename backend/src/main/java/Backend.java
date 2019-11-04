aa2f191b943ebd4e959a8e54f318316eba7bca4e
OliverLoenning <Oliver.S.Loenning@gmail.com>
2019-11-04 23:31:23 +0100

    more stuff

--- backend/src/main/java/Backend.java
+++ backend/src/main/java/Backend.java
@@ -1,5 +1,8 @@
+import DTOs.CarDetails;
+
 public class Backend {
     public static void main(String[] args) {
-        System.out.println("Hello from Backend!");
+        System.out.println("Hello my friends, embrace the magic about to come and see me console log an object from our contract. Weuhuuu");
+        System.out.println(new CarDetails("My name is Jeff", null, null));
     }
 }
